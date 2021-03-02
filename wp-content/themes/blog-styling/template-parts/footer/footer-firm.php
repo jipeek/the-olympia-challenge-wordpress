ca868b87980176d165881ec3be295cc50e1bb7ef
SebasTyler <ibikatti@gmail.com>
2021-03-02 17:35:58 -0400

    arreglos en css de gutenberg

--- wp-content/themes/blog-styling/template-parts/footer/footer-firm.php
+++ wp-content/themes/blog-styling/template-parts/footer/footer-firm.php
@@ -9,5 +9,5 @@
 
 <div class="m-footer__firm">
     <p class="a-footer__copyright"><?php echo '© '. date('Y') . ' Jipeek'; ?></p>
-    <p class="a-footer__dev">Hecho con <a href="https://hacktzi.com" class="-monlogo" target="_blank">❤</a></p>
+    <p class="a-footer__dev">Hecho con ❤</p>
 </div>
